48b44c8562d27b8673d5161f9b788f88c01d7c91
ando <terbu102@gmail.com>
2024-01-04 21:58:00 +0300

    setting up pycode

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """pascal triangle"""
 
+
 def pascal_triangle(n):
     """
     a function that return pascal triangle.
     """
-    
     if n <= 0:
         return []
-    
+
     triangle = []
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
-    return triangle          
+    return triangle
