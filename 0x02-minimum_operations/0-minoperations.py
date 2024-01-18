c2ab471b582cfd498b375e200d981fdc638d33c8
ando <terbu102@gmail.com>
2024-01-18 20:43:56 +0300

    minimum op

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""minimum operations"""
+
+def minOperations(N):
+    """return number to rech N"""
+    if N <= 1:
+        return 0
+    else:
+        for i in range(2, N + 1):
+            if N % i == 0:
+                return minOperations(N // i) + i
+        
+   
---
+++ 0x02-minimum_operations/main.py
@@ -0,0 +1,7 @@
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
