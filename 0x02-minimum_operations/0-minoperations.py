7e2e03081ca53069c5c8feb6368bf5613bf03112
ando <terbu102@gmail.com>
2024-01-18 20:46:21 +0300

    min

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,12 +2,12 @@
 """minimum operations"""
 
 
-def minOperations(N):
+def minOperations(n):
     """return number to rech N"""
-    if N <= 1:
+    if n <= 1:
         return 0
     else:
-        for i in range(2, N + 1):
-            if N % i == 0:
-                return minOperations(N // i) + i
+        for i in range(2, n + 1):
+            if n % i == 0:
+                return minOperations(n // i) + i
         
