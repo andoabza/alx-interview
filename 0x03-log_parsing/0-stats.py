80eefa56535dffcf17db0ed56a6ad0fdb2cccdaf
ando <terbu102@gmail.com>
2024-01-28 11:28:18 +0300

    handle signal

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
 '''parse log in stdin'''
 import sys
+import signal
 
 
-if __name__ == "__main__":
-    status = {'200': 0, '301': 0, '400': 0, '401': 0,
-              '403': 0, '404': 0, '405': 0, '500': 0}
-    count = 0
-    size = 0
-    try:
-        for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                size += int(data[-1])
-            except:
-                pass
-            try:
-                if data[-2] in status:
-                    status[data[-2]] += 1
-            except:
-                pass
-            if count % 10 == 0:
-                print("File size: {}".format(size))
-                for key in sorted(status.keys()):
-                    if status[key] != 0:
-                        print("{}: {}".format(key, status[key]))
-    except KeyboardInterrupt:
-        print("File size: {}".format(size))
-        for key in sorted(status.keys()):
-            if status[key] != 0:
-                print("{}: {}".format(key, status[key]))
-        raise
+count = 0
+size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+def print_stats():
+    '''print stats'''
     print("File size: {}".format(size))
-    for key in sorted(status.keys()):
-        if status[key] != 0:
-            print("{}: {}".format(key, status[key]))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+def signal_handler(sig, frame):
+    '''handle signal'''
+    print_stats()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+try:
+    for line in sys.stdin:
+        parts = line.split()
+        if len(parts) == 9:
+            try:
+                status_code = int(parts[7])
+                file_size = int(parts[8])
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+                size += file_size
+                count += 1
+                if count % 10 == 0:
+                    print_stats()
+            except ValueError:
+                continue
+except KeyboardInterrupt:
+    pass
+
+print_stats()
