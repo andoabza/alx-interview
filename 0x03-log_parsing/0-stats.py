c3c5ac45ac4b57f0cfe0aa83f9bf5aef4e90df57
ando <terbu102@gmail.com>
2024-01-28 10:37:47 +0300

    reads stdin line by line

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
-'''parse log file and print stats'''
+'''parse log in stdin'''
 import sys
-import signal
-import re
-from collections import defaultdict
 
 
-total_size = 0
-status_counts = defaultdict(int)
-lines_processed = 0
-
-
-def handle_interrupt(signum, frame):
-    # Print statistics and exit on interrupt
-    print("Total file size: ", total_size)
-    for status_code, count in status_counts.items():
-        print(f"{status_code}: {count}")
-    sys.exit()
-
-signal.signal(signal.SIGINT, handle_interrupt)
-
-for line in sys.stdin:
-    lines_processed += 1
-    match = re.match(r'(\d+\.\d+\.\d+\.\d+) - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)', line)
-    if match:
-        ip_address, status_code, file_size = match.groups()
-        total_size += int(file_size)
-        status_counts[status_code] += 1
-        if lines_processed % 10 == 0:
-            print("Total file size: ", total_size)
-            for status_code, count in status_counts.items():
-                print(f"{status_code}: {count}")
+if __name__ == "__main__":
+    status = {'200': 0, '301': 0, '400': 0, '401': 0,
+              '403': 0, '404': 0, '405': 0, '500': 0}
+    count = 0
+    size = 0
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                size += int(data[-1])
+            except:
+                pass
+            try:
+                if data[-2] in status:
+                    status[data[-2]] += 1
+            except:
+                pass
+            if count % 10 == 0:
+                print("File size: {}".format(size))
+                for key in sorted(status.keys()):
+                    if status[key] != 0:
+                        print("{}: {}".format(key, status[key]))
+    except KeyboardInterrupt:
+        print("File size: {}".format(size))
+        for key in sorted(status.keys()):
+            if status[key] != 0:
+                print("{}: {}".format(key, status[key]))
+        raise
+    print("File size: {}".format(size))
+    for key in sorted(status.keys()):
+        if status[key] != 0:
+            print("{}: {}".format(key, status[key]))
