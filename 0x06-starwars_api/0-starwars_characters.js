4c572aeb21ec52a8b2ed952aa930d2937334d387
ando <terbu102@gmail.com>
2024-02-15 17:35:25 +0300

    semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,27 @@
-#!/usr/bin/node
-/* star wars api*/
-const request =  require('request');
-var arg = process.argv[1];
-const url = "https://swapi-api.alx-tools.com/api/films/" + arg;
+//* #!/usr/bin/node
+/* star wars api */
+const request = require('request');
+const arg = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + arg;
 
-const character_url = () => {
-    request({uri: url, json:true}, function(err, resp, body){
-    let char = body.characters;
-    for (let i = 0; i in char; i++) {
-        let characters_url = char[i];
-        request({uri: characters_url, json:true}, function(err, resp, body){
-            let name = body.name;
-            console.log(name);
-        });
-    
+const CharacterUrl = () => {
+  request({ uri: url, json: true }, function (error, response, body) {
+    if (error) {
+      console.log('');
     }
-});
-}
-setTimeout(character_url, 15)
-character_url()
+    console.log(error);
+    const char = body.characters;
+    for (let i = 0; i in char; i++) {
+      const charactersUrl = char[i];
+      request({ uri: charactersUrl, json: true }, function (error, response, body) {
+        if (error) {
+          console.log('');
+        }
+        const name = body.name;
+        console.log(name);
+      });
+    }
+  });
+};
+setTimeout(CharacterUrl, 15);
+CharacterUrl();
