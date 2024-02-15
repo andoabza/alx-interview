ed70cc108ab239eede94e088360b7ecbb859b8a4
ando <terbu102@gmail.com>
2024-02-15 20:50:56 +0300

    return name

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,16 @@
-#!/usr/bin/node
+//*#!/usr/bin/node
 /* star wars api */
 const request = require('request');
-const arg = process.argv[2];
+//const arg = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + arg;
 
 const CharacterUrl = () => {
   request({ uri: url, json: true }, function (error, response, body) {
-    if (error) {
-      console.log('');
-    }
     console.log(error);
     const char = body.characters;
     for (let i = 0; i in char; i++) {
       const charactersUrl = char[i];
       request({ uri: charactersUrl, json: true }, function (error, response, body) {
-        if (error) {
-          console.log('');
-        }
         const name = body.name;
         console.log(name);
       });
