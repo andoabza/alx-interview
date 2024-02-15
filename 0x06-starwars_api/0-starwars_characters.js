1f2005d8d5c855b3419ea7f80021bf4ad8fae839
ando <terbu102@gmail.com>
2024-02-15 20:58:11 +0300

    status code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,29 @@
-#!/usr/bin/node
+//*#!/usr/bin/node
 /* star wars api */
 const request = require('request');
 
-function getStarWarsCharacters(movieId) {
-    const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+function getStarWarsCharacters (movieId) {
+  const url = `https://swapi-api.alx-tools.com/api/films/3/`;
 
-    request({ url: url, json: true }, (error, response, body) => {
-        if (!error && response.statusCode ===  200) {
-            if (body.title) {
-                body.characters.forEach((characterUrl, index) => {
-                    request({ url: characterUrl, json: true }, (error, response, characterBody) => {
-                        if (!error && response.statusCode ===  200) {
-                            console.log(`Character ${index +  1}: ${characterBody.name}`);
-                        } else {
-                            console.error(`Error fetching character ${index +  1}:`, error);
-                        }
-                    });
-                });
+  request({ url, json: true }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      if (body.title) {
+        body.characters.forEach((characterUrl, index) => {
+          request({ url: characterUrl, json: true }, (error, response, characterBody) => {
+            if (!error && response.statusCode === 200) {
+              console.log(characterBody.name);
             } else {
-                console.error('No movie found with the given ID.');
+              console.error(`Error fetching character ${index + 1}:`, error);
             }
-        } else {
-            console.error('Error fetching movie data:', error);
-        }
-    });
+          });
+        });
+      } else {
+        console.error('No movie found with the given ID.');
+      }
+    } else {
+      console.error('Error fetching movie data:', error);
+    }
+  });
 }
 
 getStarWarsCharacters(process.argv[2]);
