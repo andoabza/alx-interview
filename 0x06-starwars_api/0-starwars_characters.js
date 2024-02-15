200cdd23129085efc7b46020922efd7589da2a49
ando <terbu102@gmail.com>
2024-02-15 20:53:35 +0300

    starwar api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,32 @@
 #!/usr/bin/node
 /* star wars api */
 const request = require('request');
-const arg = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/' + arg;
 
-const CharacterUrl = () => {
-  request({ uri: url, json: true }, function (error, response, body) {
-    console.log(error);
-    const char = body.characters;
-    for (let i = 0; i in char; i++) {
-      const charactersUrl = char[i];
-      request({ uri: charactersUrl, json: true }, function (error, response, body) {
-        const name = body.name;
-        console.log(name);
-      });
-    }
-  });
-};
-setTimeout(CharacterUrl, 15);
-CharacterUrl();
+function getStarWarsCharacters(movieId) {
+    const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+    request({ url: url, json: true }, (error, response, body) => {
+        if (!error && response.statusCode ===  200) {
+            // Check if the movie exists
+            if (body.title) {
+                console.log(`Movie Title: ${body.title}`);
+                // Fetch each character URL and print the character name
+                body.characters.forEach((characterUrl, index) => {
+                    request({ url: characterUrl, json: true }, (error, response, characterBody) => {
+                        if (!error && response.statusCode ===  200) {
+                            console.log(`Character ${index +  1}: ${characterBody.name}`);
+                        } else {
+                            console.error(`Error fetching character ${index +  1}:`, error);
+                        }
+                    });
+                });
+            } else {
+                console.error('No movie found with the given ID.');
+            }
+        } else {
+            console.error('Error fetching movie data:', error);
+        }
+    });
+}
+
+getStarWarsCharacters(process.argv[2]);
